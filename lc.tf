361d29e03dac1e5277b1e79da6d862eff5b53c35
Salvian Reynaldi <salvian@traveloka.com>
2018-03-15 17:14:32 +0700

    add cluster-role variable

--- lc.tf
+++ lc.tf
@@ -1,13 +1,13 @@
 resource "aws_launch_configuration" "app" {
-  name_prefix   = "${var.service-name}-app-${var.environment}-"
-  image_id      = "${var.instance-ami-id}"
-  instance_type = "${var.instance-type}"
+  name_prefix          = "${var.service-name}-${var.cluster-role}-${var.environment}-"
+  image_id             = "${var.instance-ami-id}"
+  instance_type        = "${var.instance-type}"
   iam_instance_profile = "${var.instance-profile}"
-  key_name = "${var.instance-key-name}"
-  security_groups = ["${var.instance-sgs}"]
-  user_data = "${var.instance-user-data}"
-  enable_monitoring = "${var.instance-monitoring}"
-  ebs_optimized = "${var.instance-ebs-optimized}"
+  key_name             = "${var.instance-key-name}"
+  security_groups      = ["${var.instance-sgs}"]
+  user_data            = "${var.instance-user-data}"
+  enable_monitoring    = "${var.instance-monitoring}"
+  ebs_optimized        = "${var.instance-ebs-optimized}"
 
   lifecycle {
     create_before_destroy = true
--- variables.tf
+++ variables.tf
@@ -1,96 +1,101 @@
 variable "service-name" {
-  type    = "string"
+  type        = "string"
   description = "the name of the service"
 }
 
+variable "cluster-role" {
+  default     = "app"
+  description = "primary role/function of the cluster"
+}
+
 variable "environment" {
-  type    = "string"
+  type        = "string"
   description = "the created resources will belong to this infrastructure environment"
 }
 
 variable "asg-vpc-zone-identifier" {
-    type = "list"
-    description = "the created ASGs will spawn instances to these subnet IDs"
+  type        = "list"
+  description = "the created ASGs will spawn instances to these subnet IDs"
 }
 
 variable "asg-lb-target_group-arn" {
-    type = "string"
-    description = "the created ASGs will be attached to this target group"
+  type        = "string"
+  description = "the created ASGs will be attached to this target group"
 }
 
 variable "asg-min-capacity" {
-    default = 1
-    description = "the created ASGs will have this number of instances at minimum"
+  default     = 1
+  description = "the created ASGs will have this number of instances at minimum"
 }
 
 variable "asg-desired-capacity" {
-    type = "string"
-    description = "the created ASGs will be spawned initially with this capacity"
+  type        = "string"
+  description = "the created ASGs will be spawned initially with this capacity"
 }
 
 variable "asg-max-capacity" {
-    default = 5
-    description = "the created ASGs will have this number of instances at maximum"
+  default     = 5
+  description = "the created ASGs will have this number of instances at maximum"
 }
 
 variable "asg-wait-timeout" {
-    type = "string"
-    description = "A maximum duration that Terraform should wait for ASG instances to be healthy before timing out"
+  type        = "string"
+  description = "A maximum duration that Terraform should wait for ASG instances to be healthy before timing out"
 }
 
 variable "asg-health-check-type" {
-    type = "string"
-    default = "ELB"
-    description = "controls how ASG health checking is done"
+  type        = "string"
+  default     = "ELB"
+  description = "controls how ASG health checking is done"
 }
 
 variable "asg-health-check-grace-period" {
-    type = "string"
-    default = "300"
-    description = "Time, in seconds, to wait for new instances before checking their health"
+  type        = "string"
+  default     = "300"
+  description = "Time, in seconds, to wait for new instances before checking their health"
 }
 
 variable "asg-tags" {
-    default = []
-    description = "the created ASGs will have these tags"
+  default     = []
+  description = "the created ASGs will have these tags"
 }
 
 variable "instance-sgs" {
-    type = "list"
-    description = "the created instances will spawn instances with these security groups"
+  type        = "list"
+  description = "the created instances will spawn instances with these security groups"
 }
 
 variable "instance-profile" {
-    type = "string"
-    description = "the created instances will spawn instances with this IAM profile"
+  type        = "string"
+  description = "the created instances will spawn instances with this IAM profile"
 }
 
 variable "instance-key-name" {
-    default = ""
-    description = "the created instances will spawn instances with this SSH key name"
+  default     = ""
+  description = "the created instances will spawn instances with this SSH key name"
 }
 
 variable "instance-type" {
-    type = "string"
-    description = "the created instances will spawn instances with this type"
+  type        = "string"
+  description = "the created instances will spawn instances with this type"
 }
 
 variable "instance-ami-id" {
-    type = "string"
-    description = "the created instances will spawn instances with this AMI"
+  type        = "string"
+  description = "the created instances will spawn instances with this AMI"
 }
 
 variable "instance-monitoring" {
-    default = true
-    description = "the created instances will spawn instances with enhanced monitoring if enabled"
+  default     = true
+  description = "the created instances will spawn instances with enhanced monitoring if enabled"
 }
 
 variable "instance-ebs-optimized" {
-    default = true
-    description = "the created instances will spawn EBS-optimized instances if enabled"
+  default     = true
+  description = "the created instances will spawn EBS-optimized instances if enabled"
 }
 
 variable "instance-user-data" {
-    default = ""
-    description = "The user data to launch instances with. Use the value of data.template_cloudinit_config.rendered" // https://www.terraform.io/docs/providers/template/d/cloudinit_config.html#rendered
+  default     = ""
+  description = "The user data to launch instances with. Use the value of data.template_cloudinit_config.rendered" // https://www.terraform.io/docs/providers/template/d/cloudinit_config.html#rendered
 }
